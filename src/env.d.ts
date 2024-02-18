5588c8b4fb01db363873e34bb6bfbdf47077a648
Ashley <an26k@protonmail.com>
2024-02-17 20:02:38 -0600

    Populated contact-me screen, implemented recaptcha and added .env variables to store its keys

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly PUBLIC_RECAPTCHA_CLIENT_KEY: string;
+    readonly RECAPTCHA_SECRET_KEY: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
