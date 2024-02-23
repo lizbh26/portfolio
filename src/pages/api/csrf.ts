3862d04a41f097d0d18a52e0f2d4614c33a43b78
Ashley <an26k@protonmail.com>
2024-02-23 13:27:43 -0600

    Added prerender = false

--- src/pages/api/csrf.ts
+++ src/pages/api/csrf.ts
@@ -2,6 +2,8 @@ import Tokens from 'csrf'
 
 import type { APIContext } from 'astro'
 
+export const prerender = false
+
 export function GET({ request }: APIContext) {
 	const csrf = new Tokens()
 	const secret = csrf.secretSync()
