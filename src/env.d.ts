2b5e5568c14f747a62132d39e2a0bc68c48556a3
Ashley <an26k@protonmail.com>
2024-03-03 11:45:22 -0600

    Reverted to before implementing email form

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-	readonly PUBLIC_RECAPTCHA_CLIENT_KEY: string
-	readonly RECAPTCHA_SECRET_KEY: string
-	readonly EMAIL_USER: string
-	readonly EMAIL_PASSWORD: string
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv
-}
