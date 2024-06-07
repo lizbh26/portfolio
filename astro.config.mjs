78330876a039af3d0c55a8010327af353bacca72
Ashley <an26k@protonmail.com>
2024-06-07 16:43:02 -0600

    Added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ashley-lizbeth.github.io',
-	base: "/portfolio",
-	integrations: [tailwind()],
-	trailingSlash: 'always',
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en", "es"],
-		routing: {
-			prefixDefaultLocale: true
-		}
-	}
-})
+  site: 'https://ashley-lizbeth.github.io',
+  base: "/portfolio",
+  integrations: [tailwind(), svelte()],
+  trailingSlash: 'always',
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  }
+});
