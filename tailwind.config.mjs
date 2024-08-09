9b658b666a65d9923ea35ae83158259e929c7824
Ashley <ashley.liz2605@gmail.com>
2024-08-08 18:04:18 -0600

    Added custom colors to tailwind file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#e11d48",
+				secondary: "#fb7185",
+				background: "#171717"
+			}
+		},
 	},
 	plugins: [],
 }
